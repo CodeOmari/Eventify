ef640c7a957f0b5489170f43289ce0ca92e06c97
Jackson Omari <jacksonomari2230@gmail.com>
2024-12-13 22:03:40 +0300

    Update admin.py

--- main_app/admin.py
+++ main_app/admin.py
@@ -8,6 +8,8 @@ admin.site.register(Events)
 
 admin.site.register(Registration)
 
+admin.site.register(Payments)
+
 admin.site.site_header = 'Eventify Administration'
 
 admin.site.site_title = 'Eventify Administration'
