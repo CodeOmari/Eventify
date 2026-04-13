126a69b6bd0392bc9a5b421a21a2916200eb617b
CodeOmari <jacksonomari2230@gmail.com>
2026-04-13 17:33:04 +0300

    Add views for handling event searching, sending ticket and password reset emails

--- Eventify/main_app/views.py
+++ Eventify/main_app/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
+from django.utils import timezone
 from django.utils.timezone import now
 from datetime import timedelta
 from main_app.models import Event
 
-from main_app.app_forms import EventForm, LoginForm
+from main_app.app_forms import CustomUserCreationForm, EventForm, LoginForm, TicketForm, PasswordResetRequestForm, SetNewPasswordForm
 from django.contrib import messages
 
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+
+from django.core.mail import EmailMultiAlternatives
+from django.conf import settings
+
+from django.db.models import Q
+
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.core.signing import TimestampSigner, SignatureExpired, BadSignature
+from django.contrib.auth.hashers import make_password
+
+signer = TimestampSigner()
 
 # Create your views here.
 def events(request):
@@ -72,16 +85,195 @@ def signout_user(request):
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account creation for {username} was successful!')
             return redirect('login')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'register_form.html', {'form': form})
+
 
 def event_details(request, id):
     event = get_object_or_404(Event, id=id)
     return render(request, 'event_details.html', {'event': event})
+
+def search_event(request):
+    search_term = request.GET.get('search')
+    data= Event.objects.filter(Q(event_name__icontains=search_term) | Q(organizer__icontains=search_term)
+                                 | Q(event_location__icontains=search_term) | Q(event_type__icontains=search_term))
+    return render(request, 'event_opener.html', {"data": data, "search_term": search_term})
+
+
+@login_required
+def get_ticket(request, id):
+    event = get_object_or_404(Event, id=id)
+    
+    if request.method == "POST":
+        form = TicketForm(request.POST, request.FILES)
+    
+
+        if event.booked_slots  >= event.event_slots:
+                messages.error(request, "This event is fully booked.")
+                return redirect("events")
+        
+        if form.is_valid():
+            ticket = form.save(commit=False)
+            ticket.event = event
+            ticket.save()
+
+            event.booked_slots += ticket.tickets
+            event.save()
+
+            html_content = f"""
+                <div>
+                    <h2 style='color: #3542D4; text-align: center;'> Eventify </h2>
+
+                    <p> Hi <strong>{ticket.full_name}</strong>, </p>
+
+                    <p>
+                         You have successfully booked a ticket. Find event details attached below:
+                    </p>
+
+                    <h3>Event Details</h3>
+
+                    <ul>
+                        <li><strong> Ticket ID: </strong> 
+                            #{ticket.id}
+                        </li>
+                        <li><strong> Number of Ticket(s): </strong> 
+                            {ticket.tickets}
+                        </li>
+                        <li><strong> Event: </strong> 
+                            {event.event_name}
+                        </li>
+                        <li><strong> Location: </strong>
+                            {event.event_location}
+                        </li>
+                        <li><strong>  Date & Time: </strong>
+                            {event.event_date} &middot; {event.start_time.strftime("%I:%M %p")}
+                        </li>
+                        <li><strong> Price: </strong> 
+                            { "Free" if event.is_free == True else f'Ksh {event.event_price} per ticket' }
+                        </li>
+                    </ul>
+
+                    <p>
+                        Please present this email at the event entrance. If your ticket has a fee,
+                        payment shall be done at the entrance.
+                    </p>
+
+                    <hr>
+
+                    <p>
+                        &copy; {timezone.now().year} Eventify. All rights reserved.
+                    </p>
+                </div>
+            """
+
+            subject = "Your Event Ticket"
+            from_email = settings.DEFAULT_FROM_EMAIL
+            to = [ticket.email]
+
+            message = EmailMultiAlternatives(subject, "", from_email, to)
+            message.attach_alternative(html_content, "text/html")
+            message.send()
+
+            messages.success(request, "Ticket booked successfully! A confirmation email has been sent to your email.")
+            return redirect("events")
+    else:
+        form = TicketForm()
+    return render(request, 'ticket_form.html', {"form":form, "event":event})
+
+
+
+def password_reset_request(request):
+    if request.method == "POST":
+        form = PasswordResetRequestForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            try:
+                user = User.objects.get(email=email)
+                token = signer.sign(user.pk) # sign user id
+                reset_link = request.build_absolute_uri(
+                    reverse('password_reset_confirm', args=[token])
+                )
+
+                html_content = f"""
+                    <div>
+                        <h2 style='color: #3542D4; text-align: center;'> Eventify </h2>
+
+                        <p> Hi <strong>{user.username}</strong>, </p>
+
+                        <p>
+                            You requested a password reset. Click the link below to set a new password:
+                        </p>
+
+                        <p style='margin: 20px 0;'>
+                            <a href="{reset_link}" style='display: inline-block; 
+                                                   padding: 10px 15px; background-color: #004f71; color: white; 
+                                                   text-decoration: none; border-radius: 5px;'>
+                                Reset Password
+                            </a>
+                        </p>
+
+                        <p>
+                            If the above button doesn't work, copy and paste the following link into your browser:
+                        </p>
+
+                        <p>
+                            <a href="{reset_link}">{reset_link}</a>
+                        </p>
+
+                        <p>
+                            The link is valid for 30 minutes only.
+                        </p>
+
+                        <hr>
+
+                        <p>
+                            If you did not request a password reset, please ignore this email. <br>
+                            &copy; {timezone.now().year} NovaCare. All rights reserved.
+                        </p>
+                    </div>
+                """
+
+                subject = "Password Reset Request"
+                from_email = settings.DEFAULT_FROM_EMAIL
+                to = [email]
+
+                message = EmailMultiAlternatives(subject, "", from_email, to)
+                message.attach_alternative(html_content, "text/html")
+                message.send()
+
+
+                messages.success(request, f'Your password reset link has been sent to {email}.')
+                return redirect('login')
+            except User.DoesNotExist:
+                messages.error(request, f'No user found with the email {email}.')
+    else:
+        form = PasswordResetRequestForm()
+    return render(request, "password_reset.html", {'form': form})
+
+
+def password_reset_confirm(request, token):
+    try:
+        user_id = signer.unsign(token, max_age=3600) # token id valid for 1 hour
+        user = User.objects.get(pk=user_id)
+    except (BadSignature, SignatureExpired, User.DoesNotExist):
+        messages.error(request, 'Invalid or expired reset link.')
+        return redirect("user_management:password_reset_request")
+
+    if request.method == "POST":
+        form = SetNewPasswordForm(request.POST)
+        if form.is_valid():
+            user_password = form.cleaned_data["new_password"]
+            user.password = make_password(user_password)
+            user.save()
+            messages.success(request, 'Password updated successfully!')
+            return redirect('login')
+    else:
+        form = SetNewPasswordForm()
+    return render(request, 'password_reset_confirm.html', {'form': form})
